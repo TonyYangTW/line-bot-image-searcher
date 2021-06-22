8fa556b4e0b0b7161d81efb47660008ce1cf0fd2
TonyYangTW <tony.881118@gmail.com>
2021-06-22 16:56:31 +0800

    Initial Version

---
+++ config.py
@@ -0,0 +1,7 @@
+# imgur key
+client_id = 'fe9cc27cece587c'
+client_secret = '313cc56da6f4c968c7b1cdea4ce2ea8f992e2a97'
+
+# line bot key
+line_channel_access_token = 'ZRQAbwfNwGCX/Tr/rvfJmdpse6/uvkeUCHvLCSBrRS7vm/jS6+IcsxkZtJssTlPWLIMcmSorvBF2mVYKxaeEliXrn26/MCBciYCvAGQ4yUKe4xiGbt8ZhfjkJ8IVSUjhf2iRJO/qD9zPn3GjCbDIBgdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '90d08bbbd5cd94c90c1a0ce17cbe4948'
---
+++ line_bot_image_search.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask
+app = Flask(__name__)
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import sqlite3
+from config import client_id, line_channel_access_token, line_channel_secret
+import pyimgur
+try:
+   import xml.etree.cElementTree as ET
+except ImportError:
+   import xml.etree.ElementTree as ET
+
+line_bot_api = LineBotApi(line_channel_access_token)
+handler = WebhookHandler(line_channel_secret)
+
+
+conn = sqlite3.connect('line_bot_image_search.db')
+c = conn.cursor()
+c.execute("""CREATE TABLE if not exists "data" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"type"	TEXT NOT NULL,
+	"state"	TEXT NOT NULL,
+	"url"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+)""")
+conn.commit()
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(JoinEvent, )
+def handle_join(event):
+    print(event)
+    conn = sqlite3.connect('line_bot_image_search.db')
+    c = conn.cursor()
+    res = "NULL"
+    
+    if event.source.type == "group":
+        c.execute('SELECT id, type, state, url FROM data WHERE id=?', [event.source.group_id])
+        cursor = c.fetchone()
+        print(cursor)
+        try:
+            c.execute("INSERT INTO data (id, type, state, url) VALUES (?,?,?,?)", [event.source.group_id, "group", "activate", "no url"])
+            res = "加入新群組!\n\n指令：\n!主動模式\n!被動模式\n!搜尋"
+        except:
+            res = "重新回到群組!\n\n指令：\n!主動模式\n!被動模式\n!搜尋"
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=res))
+
+    conn.commit()
+    conn.close()
+
+@handler.add(MessageEvent, message=(ImageMessage, TextMessage))
+def handle_message(event):
+    print(event)
+    conn = sqlite3.connect('line_bot_image_search.db')
+    c = conn.cursor()
+    
+    res = "NULL"
+    if event.source.type == "group":
+        c.execute('SELECT id, type, state, url FROM data WHERE id=?', [event.source.group_id])
+        cursor = c.fetchone()
+        #conn.commit()
+        print(cursor)
+
+        if cursor == None or cursor == []:
+            try:                
+                c.execute("INSERT INTO data (id, type, state, url) VALUES (?,?,?,?)", [event.source.group_id, "group", "activate", "no url"])
+                c.execute('SELECT id, type, state, url FROM data WHERE id=?', [event.source.group_id])
+                cursor = c.fetchone()
+                print(cursor)
+                conn.commit()
+                res = "加入新群組!\n\n指令：\n!主動模式\n!被動模式\n!搜尋"
+            except:
+                res = "重新回到群組!\n\n指令：\n!主動模式\n!被動模式\n!搜尋"
+   
+        if isinstance(event.message, TextMessage):
+            mtext = event.message.text
+            if mtext == "!主動模式":
+                c.execute('UPDATE data SET state = "activate" WHERE id = ?', [event.source.group_id])
+                res = "機器人已設為主動模式，將會為您搜尋接下來上傳的每張圖片"
+            elif mtext == "!被動模式":
+                c.execute('UPDATE data SET state = "passive" WHERE id = ?', [event.source.group_id])
+                res = """機器人已設為被動模式，輸入"!搜尋"將會為您搜尋最後上傳的圖片"""
+            elif mtext == "!搜尋":
+                if cursor[3] == "no url":
+                    res = "群組中沒有上傳過圖片，或是圖片是在機器人加入前上傳的\n請再試一次！"
+                else:
+                    url = "https://www.google.com/searchbyimage?&image_url=" + cursor[3]
+                    res = "已為您上傳並搜尋群組中最後一張圖片\n以下為本次的搜尋結果\n" + url + \
+                            "\n\n若想檢視原始圖片，請點擊下列網址\n" + cursor[3]
+                
+                
+    if isinstance(event.message, ImageMessage):
+        print("*1*")
+        message_content = line_bot_api.get_message_content(event.message.id)
+        #with tempfile.NamedTemporaryFile(suffix='.jpg', mode="wb", delete=False) as tf:
+        #with tempfile.NamedTemporaryFile(prefix=ext + '-', delete=False) as tf:
+        file_path = "/tmp/" + event.message.id + ".jpg"
+        #file_path = "a.jpg"
+        with open(file_path, 'wb') as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+
+        print(file_path)
+       
+        CLIENT_ID = client_id
+        PATH = file_path #A Filepath to an image on your computer"
+        title = "Uploaded with PyImgur"
+
+        print("*2*")
+        im = pyimgur.Imgur(CLIENT_ID)
+        uploaded_image = im.upload_image(PATH, title=title)
+
+        print("*3*")
+        print(uploaded_image.title)
+        print(uploaded_image.link)
+        print(uploaded_image.type)
+        
+        url = "https://www.google.com/searchbyimage?&image_url=" + uploaded_image.link
+        print(cursor)
+        if event.source.type == "group" and cursor[2] == "passive":
+            c.execute('UPDATE data SET url = ? where id = ?', [uploaded_image.link, event.source.group_id])
+            print("passive")
+        elif event.source.type == "group" and cursor[2] == "activate":
+            c.execute('UPDATE data SET url = ? where id = ?', [uploaded_image.link, event.source.group_id])
+            print("activate")
+            res = "已為您上傳並搜尋圖片\n以下為本次的搜尋結果\n" + url + \
+                "\n\n若想檢視原始圖片，請點擊下列網址\n" + uploaded_image.link
+        else:
+            res = "已為您上傳並搜尋圖片\n以下為本次的搜尋結果\n" + url + \
+                "\n\n若想檢視原始圖片，請點擊下列網址\n" + uploaded_image.link
+    
+    if res != "NULL":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=res))
+
+    conn.commit()
+    conn.close()
+
+   
+if __name__ == '__main__':
+    app.run()
